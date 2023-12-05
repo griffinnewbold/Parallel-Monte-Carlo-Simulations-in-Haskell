b11d4388d60a8b5aeab36a91a5268c01809e505b
Anna Christensen <annajchr@gmail.com>
2023-12-05 14:52:46 -0500

    Incomplete progress on Sequential, notes in Lib file

--- monteParSim/app/Main.hs
+++ monteParSim/app/Main.hs
@@ -3,4 +3,13 @@ module Main (main) where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do putStrLn $ show (exact:m_carlo)
+        where exact = exact_price t r u d s0 k
+              m_carlo = [(seq_monte n t r u d s0 k ) | n <- [100, 1000, 100000,10000000]::[Int]]
+              t = 10::Int
+              r = 0.05::Float
+              u = 1.15::Float
+              d = 1.01::Float
+              s0 = 50::Float
+              k = 70::Float
+              
--- monteParSim/src/Lib.hs
+++ monteParSim/src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib
-    ( someFunc
+    ( seq_monte,
+      exact_price
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+bernoulli :: Float -> Int
+bernoulli p = if random_val < p then 1 else 0
+    where random_val = (0::Float)
+-- need to get System.Random or some other import to replace random_val with a random float between 0 and 1
+
+
+seq_monte 0 0 r u d s_o k = (1::Float)
+seq_monte 0 t r u d s_o k = (1::Float)
+seq_monte n t r u d s_o k = (1::Float)
+
+
+--  I am currently working on exact price
+exact_price t r u d s_o k = p_star
+        where p_star = ((1::Float)+r-d)/(u-d)
+
+--[ s_o*u**k*d**(t::Float)-k) | k <- [0..t]]
+       
