6be1526f54525f2b29e490558922dbfa7dacfd58
Anna Christensen <annajchr@gmail.com>
2023-12-05 18:58:37 -0500

    fixed negative exponent error; exact price is outputting incorrect value

--- monteParSim/app/Main.hs
+++ monteParSim/app/Main.hs
@@ -4,11 +4,12 @@ import Lib
 
 main :: IO ()
 main = do putStrLn $ show (exact:m_carlo)
-        where exact = exact_price t r u d s0 k
-              m_carlo = [(seq_monte n t r u d s0 k ) | n <- [100, 1000, 100000,10000000]::[Int]]
-              t = 10::Int
-              r = 0.05::Float
-              u = 1.15::Float
-              d = 1.01::Float
-              s0 = 50::Float
-              k = 70::Float
+        where exact = exactPrice t r u d s0 k
+              m_carlo = []
+             -- m_carlo = [(seq_monte n t r u d s0 k ) | n <- [100, 1000, 100000,10000000]::[Int]]
+              t = 10::Integer
+              r = 0.05::Double
+              u = 1.15::Double
+              d = 1.01::Double
+              s0 = 50::Double
+              k = 70::Double
