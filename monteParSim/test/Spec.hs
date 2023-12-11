ac02b95c671a6a1f4cc0a8a78b2fdd306dcfc85a
Griffin Newbold <griffinnewbold@gmail.com>
2023-12-11 15:19:32 -0500

    Added Basic Test to confirm functionality use stack test to run

--- monteParSim/test/Spec.hs
+++ monteParSim/test/Spec.hs
@@ -1,2 +1,21 @@
+import Test.Hspec
+import Library 
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "monteCarloAsian" $ do
+    it "calculates the payoff of the option C_T after T time units" $ do
+      let n = 10000
+          t = 10
+          r = 0.05
+          u = 1.15
+          d = 1.01
+          s0 = 50
+          k = 70
+
+      result <- monteCarloAsian n t r u d s0 k
+
+      let lowerBound = 0.9 
+          upperBound = 1.4  
+
+      result `shouldSatisfy` (\x -> x >= lowerBound && x <= upperBound)
